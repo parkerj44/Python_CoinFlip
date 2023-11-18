f3523268ed5c1664317a922052b53601da8aadde
parkerj44 <johnson.7725@osu.edu>
2023-11-18 17:19:03 -0500

    Finish up methods

--- Main.py
+++ Main.py
@@ -1,10 +1,37 @@
-from math import random
+import random
 
 def coinflip():
-  heads = 0
-  tails = 0;
 
-  count = 0;
+  var = random.random()
+  
+  if var < .5:
+    result = 'Heads'
+  else:
+    result = "Tails"
 
-  while count <= 1:
-    
+  return result
+
+
+def all_heads(count):
+
+  attempts = 0 
+  result = False
+
+  while result == False:
+    i = 0
+    flip = 'Heads'
+    while i < count and flip == 'Heads':
+      flip = coinflip()
+      i+=1
+      if i == count - 1 and flip == 'Heads':
+        result = True
+
+    attempts += 1
+
+  print("It took {} attempts to get {} Heads in a row.".format(attempts, count))
+
+
+print("This game checks how many times it takes to get n amount of heads in a row.")
+user_choice = input("Enter a number to test: ")
+
+all_heads(user_choice)
